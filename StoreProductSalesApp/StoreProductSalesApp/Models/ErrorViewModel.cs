5ac057bd69b64a2d669f66d2a6846837bc7a9212
Fernando <ferescoto98@gmail.com>
2020-12-11 20:46:23 -0800

    Initial commit

---
+++ StoreProductSalesApp/StoreProductSalesApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StoreProductSalesApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
